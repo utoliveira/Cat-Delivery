2dfad73ea58ede86541a4f59e2f07e46de2f7704
Higor Oliveira <hosoares17@gmail.com>
2021-12-07 17:18:55 -0300

    Player able to jump in the air

--- Cat Delivery/Assets/Scripts/Character2DMovement.cs
+++ Cat Delivery/Assets/Scripts/Character2DMovement.cs
@@ -19,13 +19,19 @@ public class Character2DMovement : MonoBehaviour
     private bool isfacingRight = true; 
 
     private bool isGrounded = false;
+    private bool jumpedTwice = false;
+
+    private bool canJumpInTheAir = true; 
+
 
     private void FixedUpdate() {
         bool wasPlayerGrounded = isGrounded;
         isGrounded = CheckPlayerGrounded();
 
-        if(isGrounded && !wasPlayerGrounded)
+        if(isGrounded && !wasPlayerGrounded){
             onLandEvent.Invoke();
+            jumpedTwice = false;
+        }
     }
 
     private bool CheckPlayerGrounded (){
@@ -34,22 +40,31 @@ public class Character2DMovement : MonoBehaviour
 
     public void Move(float horizontalMove, bool jump, bool moveDown) {
         Vector2 targetVelocity = new Vector2(horizontalMove * 10f * speed * Time.deltaTime, rigidBody2D.velocity.y);
-        //rigidBody2D.velocity = Vector2.Lerp(rigidBody2D.velocity, targetVelocity, speed );
         rigidBody2D.velocity = Vector2.SmoothDamp(rigidBody2D.velocity, targetVelocity, ref velocity, velocity_smoothing );
         
         if(jump && isGrounded){
             rigidBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-
-        if(moveDown && isGrounded){
+    
+        if(moveDown && isGrounded)
             DeactivateColliderBelow();
+        
+        if(jump && isAbleToJumpInTheAir()){
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, 0);
+            rigidBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpedTwice = true;
         }
 
-        if(isfacingRight && horizontalMove < 0){
+        if(isfacingRight && horizontalMove < 0)
             Flip();
-        }else if(!isfacingRight &&  horizontalMove > 0){
+
+        else if(!isfacingRight &&  horizontalMove > 0)
             Flip();
-        }
+        
+    }
+
+    private bool isAbleToJumpInTheAir(){
+        return canJumpInTheAir && !isGrounded && !jumpedTwice;
     }
 
     private void DeactivateColliderBelow(){
