43a79e29ffdb2681d0f800efb034bc53e7d7227a
Higor Oliveira <hosoares17@gmail.com>
2021-12-08 14:31:43 -0300

    Fixing AlwaysOnCamera to better item desired marker position

--- Cat Delivery/Assets/AlwaysOnCamera.cs
+++ Cat Delivery/Assets/AlwaysOnCamera.cs
@@ -3,38 +3,58 @@ using UnityEngine;
 public class AlwaysOnCamera : MonoBehaviour {
     private Vector3 initialPosition;
     private RectTransform viewport;
-    float limitX;
-    float limitY;
+    private Transform playerTransform;
+    float viewportLimitX;
+    float viewportLimitY;
+
+    float limitDistance = 6;
+    [SerializeField] float movementSpeed = 15;
+    
 
     private void Awake() {
         this.viewport = GameObject.Find("Always On Screen Space").GetComponent<RectTransform>();
+        playerTransform = GameObject.FindGameObjectWithTag(Tags.PLAYER).transform;
     }
     private void Start() {
         initialPosition = this.transform.position;
-        limitX = viewport.rect.width /2 * viewport.transform.lossyScale.x;
-        limitY = viewport.rect.height /2 * viewport.transform.lossyScale.y; 
+        viewportLimitX = viewport.rect.width /2 * viewport.transform.lossyScale.x;
+        viewportLimitY = viewport.rect.height /2 * viewport.transform.lossyScale.y; 
     }
     
     private void LateUpdate() {
-        Vector2 distanceToItem = initialPosition - viewport.position;
-
         Vector2 destination = initialPosition;
-        
-        if(Mathf.Abs(distanceToItem.x) > limitX)
-            destination.x = distanceToItem.x > 0 ? viewport.position.x + limitX : viewport.position.x -limitX;
-             
-        if(Mathf.Abs(distanceToItem.y) > limitY)
-            destination.y = distanceToItem.y > 0 ? viewport.position.y + limitY: viewport.position.y -limitY;
-        
 
-        //Change to two parts: inside circle and the canvas limit
-        Debug.DrawLine(viewport.position, new Vector2(viewport.position.x + limitX, viewport.position.y + limitY), Color.yellow);
-        Debug.DrawLine(viewport.position, destination, Color.green);
-        this.transform.position = Vector2.Lerp(this.transform.position, destination, 15 * Time.deltaTime);
+        if(!IsObjectInsideViewport())
+            destination = GetNewPointCloserToPlayer();
+
+        Debug.DrawLine(playerTransform.position, destination, Color.green);
+        this.transform.position = Vector2.Lerp(this.transform.position, destination, movementSpeed * Time.deltaTime);
+    }
+
+    private bool IsObjectInsideViewport(){
+        Vector2 distanceToItem = initialPosition - viewport.position;
+        
+        return Mathf.Abs(distanceToItem.x) <= viewportLimitX 
+                && Mathf.Abs(distanceToItem.y) <= viewportLimitY;
     }
     private void OnDrawGizmos() {
-        if(!viewport) return;
+        if(!playerTransform) return;
         Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(playerTransform.position, limitDistance);
+    }
+
+    private Vector2 GetNewPointCloserToPlayer(){
+        float distanceFromObject = Vector2.Distance(playerTransform.position, initialPosition);
+        
+        float coefDistance = limitDistance/distanceFromObject;
+
+        float x = GetNewPointValueBetween(coefDistance, playerTransform.position.x, initialPosition.x);
+        float y = GetNewPointValueBetween(coefDistance, playerTransform.position.y,initialPosition.y);
+        return new Vector2(x, y);
+    }
+
+    private float GetNewPointValueBetween(float coefDistance, float playerPoint, float initialPoint){
+        return (1 - coefDistance) * playerPoint + coefDistance * initialPoint;
     }
 
 }
